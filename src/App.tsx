8f81e65f0fcd211220e77d39917e3616f7513b00
Halmos Tamás <halmos.tamas001@gmail.com>
2026-03-19 19:46:34 +0100

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react"
+
+import DarkMode from "@/components/DarkMode/DarkMode"
+
+  import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger
+} from "@/components/ui/menubar"
+
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  const [running, setRunning] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+
+
+
+  const start = async () => {
+    await fetch("http://localhost:8000/start", { method: "POST" })
+    setRunning(true)
+  }
+
+  const stop = async () => {
+    await fetch("http://localhost:8000/stop", { method: "POST" })
+    setRunning(false)
+    fetchTotal()
+  }
+
+  const fetchTotal = async () => {
+    const res = await fetch("http://localhost:8000/today")
+    const data = await res.json()
+    setSeconds(data.seconds)
+  }
+
+  useEffect(() => {
+    fetchTotal()
+  }, [])
+
+return (
+  <>
+    {/* FELSŐ SÁV */}
+    <Menubar className="flex-1 justify-center ghost">
+      
+      <MenubarMenu>
+        <MenubarTrigger>Timer</MenubarTrigger>
+        <MenubarTrigger>Stats</MenubarTrigger>
+        <MenubarTrigger>Valami</MenubarTrigger>
+        
+      </MenubarMenu>
+
+    </Menubar>
+    <DarkMode/>
+    
+    {/* TARTALOM */}
+    
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Time Tracker</h1>
+
+      <Button className= "ghost" onClick={running ? stop : start}>
+        {running ? "stop" : "start"}
+      </Button>
+
+      <h2 className="mt-4">
+        Today total:{seconds} sec 
+      </h2>
+    </div>
+  </>
+)
+}
---
+++ src/components/DarkMode/DarkMode.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import Sun from "./Sun.svg"
+import Moon from "./Moon.svg"
+import "./DarkMode.css"
+
+export default function DarkMode() {
+  const [isDark, setIsDark] = useState(false)
+
+  const setDarkMode = () => {
+    document.body.classList.add("dark")
+  }
+
+  const setLightMode = () => {
+    document.body.classList.remove("dark")
+  }
+
+  const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = e.target.checked
+    setIsDark(checked)
+
+    if (checked) {
+      setDarkMode()
+    } else {
+      setLightMode()
+    }
+  }
+
+  return (
+    <div className="dark_mode">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        checked={isDark}
+        onChange={toggleTheme}
+      />
+
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
+        <img src={Sun} className="sun" />
+        <img src={Moon} className="moon" />
+      </label>
+    </div>
+  )
+}
